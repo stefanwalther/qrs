853a8c4be33ce21c7910cd22a8d5682481fd0a76
Stefan Walther <swr@qlik.com>
2015-03-11 10:16:26 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * qrs <https://github.com/stefanwalther/qrs>
+ *
+ * Copyright (c) 2015, Stefan Walther.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
