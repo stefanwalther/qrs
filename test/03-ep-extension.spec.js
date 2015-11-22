716d3d5c0e468215452e83803cb74063d4938548
Stefan Walther <swr@qlik.com>
2015-11-22 21:30:30 +0100

    [tests] Get rid of leche in ep-extension.spec

--- test/03-ep-extension.spec.js
+++ test/03-ep-extension.spec.js
@@ -10,7 +10,7 @@ var withData = leche.withData;
 var extend = require( 'extend-shallow' );
 var fsUtils = require( 'fs-utils' );
 var path = require( 'path' );
-var setup = require( './testSetup' );
+var testSetup = require( './testSetup' );
 var async = require( 'async' );
 var extensionSetup = require( './03-ep-extension.setup' )();
 
@@ -23,175 +23,175 @@ var globalConfig = {
 };
 var qrs;
 
-describe( 'qrs.extension', function () {
+describe.only( 'qrs.extension', function () {
 
-
-	withData( setup.testLoop, function ( sessionInfo ) {
-
-		var testConfig = extend( globalConfig, sessionInfo );
-
-
-		after( function ( done ) {
-			console.log('after \n');
-			//extensionSetup.cleanExtZipFiles( function ( /*err, extensions*/ ) {
-			//	done();
-			//} );
-			done();
-		} );
-
-		before( function ( done ) {
-			console.log('before\n');
-			extensionSetup.init( done );
-		} );
-
-
-		beforeEach( function ( done ) {
-			console.log('beforeEach\n');
-			qrs = new QRS( testConfig );
-			async.map( extensionSetup.extensions, function ( ext, cb ) {
-				console.log('delete ' + ext.name);
-				qrs.extension.delete( ext.name )
-						.then( function ( data ) {
-							//cb( null );
-						}, function ( err ) {
-							cb( err );
-						});
-			}, function ( err /*, results */ ) {
-				if (err) {
-					throw err;
-				}
-				done( );
-			});
-		} );
-
-
-		it( 'should be an object', function () {
-			expect( qrs.extension ).to.exist;
-		} );
-
-		it( 'should return all installed extensions', function ( done ) {
-			qrs.extension.getInstalled()
-				.then( function ( data ) {
-					expect( data ).to.exist;
-					expect( data ).to.be.an.array;
-				}, function ( err ) {
-					expect( err ).to.not.exist;
-				} )
-				.done( function () {
-					done();
-				} );
-		} );
-
-		// Todo: To make the test repeatable & reliebable we have to upload first, then we can check the test.
-		it( 'should allow a filter to only return specific types', function ( done ) {
-			qrs.extension.getInstalled( ['visualization-template'] )
-				.then( function ( data ) {
-					expect( data ).to.exist;
-					expect( data ).to.be.an.array;
-				}, function ( err ) {
-					expect( err ).to.not.exist;
-				} )
-				.done( function () {
-					done();
-				} );
-		} );
-
-		//Todo: Improve the test
-		it( 'should return only extensions of type <visualization>', function ( done ) {
-			qrs.extension.getInstalledVis()
-				.then( function ( data ) {
-					expect( data ).to.exist;
-					expect( data ).to.be.an.array;
-				}, function ( err ) {
-					expect( err ).to.not.exist;
-				} )
-				.done( function () {
-					done();
-				} );
-		} );
-
-		//Todo: Improve the test
-		it( 'should return only extensions of type <visualization-template>', function ( done ) {
-			qrs.extension.getInstalledVisTemplates()
-				.then( function ( data ) {
-					expect( data ).to.exist;
-					expect( data ).to.be.an.array;
-				}, function ( err ) {
-					expect( err ).to.not.exist;
-				} )
-				.done( function () {
-					done();
-				} );
-		} );
-
-		//Todo: Improve the test
-		it( 'should return only extensions of type <mashup>', function ( done ) {
-			qrs.extension.getInstalledMashups()
-				.then( function ( data ) {
-					expect( data ).to.exist;
-					expect( data ).to.be.an.array;
-				}, function ( err ) {
-					expect( err ).to.not.exist;
-				} )
-				.done( function () {
-					done();
-				} );
-		} );
-
-		//Todo: Improve the test
-		it( 'should return only extensions of type <mashup-template>', function ( done ) {
-			qrs.extension.getInstalledMashupTemplates()
-				.then( function ( data ) {
-					expect( data ).to.exist;
-					expect( data ).to.be.an.array;
-				}, function ( err ) {
-					expect( err ).to.not.exist;
-				} )
-				.done( function () {
-					done();
-				} );
-		} );
-
-		it.skip( 'doesn\'t allow upload of extension with a different type than .zip', function ( done ) {
-			expect( true ).to.equal( false );
-			done();
-		} );
-
-		it.skip( 'doesn\'t allow upload of non existing files', function ( done ) {
-			expect( true ).to.equal( false );
-			done();
-		} );
-
-		it.only( 'should allow upload of an extension with absolute path', function ( done ) {
-			var extFilePath = path.join( __dirname, './fixtures/extensions/qrs-sample.zip' );
-			qrs.extension.upload( extFilePath )
-				.then( function ( data ) {
-					expect( data ).to.exist;
-				}, function ( err ) {
-					expect( err ).to.not.exist;
-				} )
-				.done( function () {
-					done();
-				} );
-		} );
-
-		describe( 'should allow deletion of an extension', function () {
-
-			before( '', function ( done ) {
-				//qrs.extension.upload
-				done();
-			} );
-			after( '', function ( done ) {
-
-				done();
-			} );
-
-			it( '...', function ( done ) {
-				expect( true ).to.equal( true );
-				done();
-			} );
-		} );
-
+	it( 'should be an object', function () {
+		qrs = new QRS( globalConfig );
+		expect( qrs.extension ).to.exist;
 	} );
 
+	testSetup.testLoop.forEach( function ( loopConfig ) {
+
+		describe( 'with ' + loopConfig.name, function (  ) {
+
+			var testConfig = extend( globalConfig, loopConfig.config );
+
+			after( function ( done ) {
+				console.log('after \n');
+				//extensionSetup.cleanExtZipFiles( function ( /*err, extensions*/ ) {
+				//	done();
+				//} );
+				done();
+			} );
+
+			before( function ( done ) {
+				console.log('before\n');
+				extensionSetup.init( done );
+			} );
+
+			beforeEach( function ( done ) {
+				console.log('beforeEach\n');
+				qrs = new QRS( testConfig );
+				async.map( extensionSetup.extensions, function ( ext, cb ) {
+					console.log('delete ' + ext.name);
+					qrs.extension.delete( ext.name )
+							.then( function ( data ) {
+								//cb( null );
+							}, function ( err ) {
+								cb( err );
+							});
+				}, function ( err /*, results */ ) {
+					if (err) {
+						throw err;
+					}
+					done( );
+				});
+			} );
+
+
+			it( 'should return all installed extensions', function ( done ) {
+				qrs.extension.getInstalled()
+						.then( function ( data ) {
+							expect( data ).to.exist;
+							expect( data ).to.be.an.array;
+						}, function ( err ) {
+							expect( err ).to.not.exist;
+						} )
+						.done( function () {
+							done();
+						} );
+			} );
+
+			// Todo: To make the test repeatable & reliebable we have to upload first, then we can check the test.
+			it( 'should allow a filter to only return specific types', function ( done ) {
+				qrs.extension.getInstalled( ['visualization-template'] )
+						.then( function ( data ) {
+							expect( data ).to.exist;
+							expect( data ).to.be.an.array;
+						}, function ( err ) {
+							expect( err ).to.not.exist;
+						} )
+						.done( function () {
+							done();
+						} );
+			} );
+
+			//Todo: Improve the test
+			it( 'should return only extensions of type <visualization>', function ( done ) {
+				qrs.extension.getInstalledVis()
+						.then( function ( data ) {
+							expect( data ).to.exist;
+							expect( data ).to.be.an.array;
+						}, function ( err ) {
+							expect( err ).to.not.exist;
+						} )
+						.done( function () {
+							done();
+						} );
+			} );
+
+			//Todo: Improve the test
+			it( 'should return only extensions of type <visualization-template>', function ( done ) {
+				qrs.extension.getInstalledVisTemplates()
+						.then( function ( data ) {
+							expect( data ).to.exist;
+							expect( data ).to.be.an.array;
+						}, function ( err ) {
+							expect( err ).to.not.exist;
+						} )
+						.done( function () {
+							done();
+						} );
+			} );
+
+			//Todo: Improve the test
+			it( 'should return only extensions of type <mashup>', function ( done ) {
+				qrs.extension.getInstalledMashups()
+						.then( function ( data ) {
+							expect( data ).to.exist;
+							expect( data ).to.be.an.array;
+						}, function ( err ) {
+							expect( err ).to.not.exist;
+						} )
+						.done( function () {
+							done();
+						} );
+			} );
+
+			//Todo: Improve the test
+			it( 'should return only extensions of type <mashup-template>', function ( done ) {
+				qrs.extension.getInstalledMashupTemplates()
+						.then( function ( data ) {
+							expect( data ).to.exist;
+							expect( data ).to.be.an.array;
+						}, function ( err ) {
+							expect( err ).to.not.exist;
+						} )
+						.done( function () {
+							done();
+						} );
+			} );
+
+			it.skip( 'doesn\'t allow upload of extension with a different type than .zip', function ( done ) {
+				expect( true ).to.equal( false );
+				done();
+			} );
+
+			it.skip( 'doesn\'t allow upload of non existing files', function ( done ) {
+				expect( true ).to.equal( false );
+				done();
+			} );
+
+			it.only( 'should allow upload of an extension with absolute path', function ( done ) {
+				var extFilePath = path.join( __dirname, './fixtures/extensions/qrs-sample.zip' );
+				qrs.extension.upload( extFilePath )
+						.then( function ( data ) {
+							expect( data ).to.exist;
+						}, function ( err ) {
+							expect( err ).to.not.exist;
+						} )
+						.done( function () {
+							done();
+						} );
+			} );
+
+			describe( 'should allow deletion of an extension', function () {
+
+				before( '', function ( done ) {
+					//qrs.extension.upload
+					done();
+				} );
+				after( '', function ( done ) {
+
+					done();
+				} );
+
+				it( '...', function ( done ) {
+					expect( true ).to.equal( true );
+					done();
+				} );
+			} );
+
+		});
+	});
 } );
--- test/testSetup.js
+++ test/testSetup.js
@@ -8,45 +8,51 @@ var TestSetup = function () {
 
 	function getTestLoop () {
 
-		var r = {};
+		var configs = [];
 
 		if ( testConfig.authentication.header.enabled ) {
-			r['Header authentication'] = [{
-				authentication: 'header',
-				headerKey: testConfig.authentication.header.headerKey,
-				headerValue: testConfig.authentication.header.headerValue,
-				virtualProxy: testConfig.authentication.header.virtualProxy,
-				port: testConfig.authentication.header.port || testConfig.port,
-				useSSL: _.isBoolean( testConfig.authentication.header.useSSL ) ? testConfig.authentication.header.useSSL : testConfig.useSSL
-			}];
-		}
-		if ( testConfig.authentication.ntlm.enabled ) {
-			r['Ntlm authentication'] = [{
-				authentication: 'ntlm',
-				virtualProxy: testConfig.authentication.ntlm.virtualProxy,
-				port: testConfig.authentication.ntlm.port || testConfig.port
-			}];
+			configs.push( {
+					'name': 'Header authentication',
+				'config': {
+					authentication: 'header',
+					headerKey: testConfig.authentication.header.headerKey,
+					headerValue: testConfig.authentication.header.headerValue,
+					virtualProxy: testConfig.authentication.header.virtualProxy,
+					port: testConfig.authentication.header.port || testConfig.port,
+					useSSL: _.isBoolean( testConfig.authentication.header.useSSL ) ? testConfig.authentication.header.useSSL : testConfig.useSSL
+				}
+			});
 		}
 
-		if ( testConfig.authentication.certificates.enabled ) {
-			r['Certificates based authentication'] = [{
-				authentication: 'certificates',
-				headerKey: testConfig.authentication.certificates.headerKey,
-				headerValue: testConfig.authentication.certificates.headerValue,
-				useSSL: _.isBoolean(testConfig.authentication.certificates.useSSL) ? testConfig.authentication.certificates.useSSL : testConfig.useSSL,
-				cert: testConfig.authentication.certificates.cert,
-				key: testConfig.authentication.certificates.key,
-				ca: testConfig.authentication.certificates.ca,
-				passphrase: testConfig.authentication.certificates.passphrase,
-				virtualProxy: testConfig.authentication.certificates.virtualProxy,
-				port: testConfig.authentication.certificates.port || testConfig.port
-			}];
-		}
-		return r;
+		//if ( testConfig.authentication.ntlm.enabled ) {
+		//	r['Ntlm authentication'] = [{
+		//		authentication: 'ntlm',
+		//		virtualProxy: testConfig.authentication.ntlm.virtualProxy,
+		//		port: testConfig.authentication.ntlm.port || testConfig.port
+		//	}];
+		//}
+		//
+		//if ( testConfig.authentication.certificates.enabled ) {
+		//	r['Certificates based authentication'] = [{
+		//		authentication: 'certificates',
+		//		headerKey: testConfig.authentication.certificates.headerKey,
+		//		headerValue: testConfig.authentication.certificates.headerValue,
+		//		useSSL: _.isBoolean(testConfig.authentication.certificates.useSSL) ? testConfig.authentication.certificates.useSSL : testConfig.useSSL,
+		//		cert: testConfig.authentication.certificates.cert,
+		//		key: testConfig.authentication.certificates.key,
+		//		ca: testConfig.authentication.certificates.ca,
+		//		passphrase: testConfig.authentication.certificates.passphrase,
+		//		virtualProxy: testConfig.authentication.certificates.virtualProxy,
+		//		port: testConfig.authentication.certificates.port || testConfig.port
+		//	}];
+		//}
+
+		return configs;
 	}
 
 	return {
-		testLoop: getTestLoop()
+		testLoop: getTestLoop(),
+		testConfig: testConfig
 	};
 
 };
