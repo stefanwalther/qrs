98d296b8531c953ad8fc0099a10d097c1896a21d
Stefan Walther <swr@qlik.com>
2015-11-23 23:59:04 +0100

    Improve tests for extensions.

--- test/e2e/03-ep-extension.e2e.spec.js
+++ test/e2e/03-ep-extension.e2e.spec.js
@@ -3,6 +3,7 @@
 'use strict';
 
 var chai = require( 'chai' );
+var chaiAsPromised = require( 'chai-as-promised' );
 var expect = chai.expect;
 var QRS = new require( './../../lib/qrs' );
 var extend = require( 'extend-shallow' );
@@ -12,10 +13,12 @@ var testSetup = require( './../testSetup' );
 var async = require( 'async' );
 var extensionSetup = require( './../03-ep-extension.setup' )();
 
+chai.use( chaiAsPromised );
+
 var qrs;
 
 var globalConfig = testSetup.globalConfig;
-describe.skip( 'qrs.extension', function () {
+describe( 'qrs.extension', function () {
 
 	it( 'should be an object', function () {
 		qrs = new QRS( globalConfig );
@@ -24,12 +27,12 @@ describe.skip( 'qrs.extension', function () {
 
 	testSetup.testLoop.forEach( function ( loopConfig ) {
 
-		describe( 'with ' + loopConfig.name, function (  ) {
+		describe( 'with ' + loopConfig.name, function () {
 
 			var testConfig = extend( globalConfig, loopConfig.config );
 
 			after( function ( done ) {
-				console.log('after \n');
+				console.log( 'after \n' );
 				//extensionSetup.cleanExtZipFiles( function ( /*err, extensions*/ ) {
 				//	done();
 				//} );
@@ -37,145 +40,128 @@ describe.skip( 'qrs.extension', function () {
 			} );
 
 			before( function ( done ) {
-				console.log('before\n');
+				console.log( 'before\n' );
 				extensionSetup.init( done );
 			} );
 
 			beforeEach( function ( done ) {
-				console.log('beforeEach\n');
+				console.log( 'beforeEach\n' );
 				qrs = new QRS( testConfig );
 				async.map( extensionSetup.extensions, function ( ext, cb ) {
-					console.log('delete ' + ext.name);
+					console.log( 'delete ' + ext.name );
 					qrs.extension.delete( ext.name )
-							.then( function ( data ) {
-								//cb( null );
-							}, function ( err ) {
-								cb( err );
-							});
+						.then( function ( data ) {
+							//cb( null );
+						}, function ( err ) {
+							cb( err );
+						} );
 				}, function ( err /*, results */ ) {
-					if (err) {
+					if ( err ) {
 						throw err;
 					}
-					done( );
-				});
+					done();
+				} );
 			} );
 
-			it( 'should return all installed extensions', function ( done ) {
-				qrs.extension.getInstalled()
-						.then( function ( data ) {
-							expect( data ).to.exist;
-							expect( data ).to.be.an.array;
-						}, function ( err ) {
-							expect( err ).to.not.exist;
-						} )
-						.done( function () {
-							done();
-						} );
+			it( 'should return all installed extensions', function ( ) {
+
+				expect( qrs.extension.getInstalled() ).to.be.fulfilled.and.to.be.an.array;
+
 			} );
 
 			// Todo: To make the test repeatable & reliebable we have to upload first, then we can check the test.
-			it( 'should allow a filter to only return specific types', function ( done ) {
-				qrs.extension.getInstalled( ['visualization-template'] )
-						.then( function ( data ) {
-							expect( data ).to.exist;
-							expect( data ).to.be.an.array;
-						}, function ( err ) {
-							expect( err ).to.not.exist;
-						} )
-						.done( function () {
-							done();
-						} );
+			it( 'should allow a filter to only return specific types', function () {
+
+				expect( qrs.extension.getInstalled( ['visualization-template'] ) ).to.be.fulfilled.and.to.be.an.array;
+
 			} );
 
 			//Todo: Improve the test => move to unit tests
 			it( 'should return only extensions of type <visualization>', function ( done ) {
 				qrs.extension.getInstalledVis()
-						.then( function ( data ) {
-							expect( data ).to.exist;
-							expect( data ).to.be.an.array;
-						}, function ( err ) {
-							expect( err ).to.not.exist;
-						} )
-						.done( function () {
-							done();
-						} );
+					.then( function ( data ) {
+						expect( data ).to.exist;
+						expect( data ).to.be.an.array;
+					}, function ( err ) {
+						expect( err ).to.not.exist;
+					} )
+					.done( function () {
+						done();
+					} );
 			} );
 
 			//Todo: Improve the test => move to unit tests
 			it( 'should return only extensions of type <visualization-template>', function ( done ) {
 				qrs.extension.getInstalledVisTemplates()
-						.then( function ( data ) {
-							expect( data ).to.exist;
-							expect( data ).to.be.an.array;
-						}, function ( err ) {
-							expect( err ).to.not.exist;
-						} )
-						.done( function () {
-							done();
-						} );
+					.then( function ( data ) {
+						expect( data ).to.exist;
+						expect( data ).to.be.an.array;
+					}, function ( err ) {
+						expect( err ).to.not.exist;
+					} )
+					.done( function () {
+						done();
+					} );
 			} );
 
 			//Todo: Improve the test ==> move to unit tests
 			it( 'should return only extensions of type <mashup>', function ( done ) {
 				qrs.extension.getInstalledMashups()
-						.then( function ( data ) {
-							expect( data ).to.exist;
-							expect( data ).to.be.an.array;
-						}, function ( err ) {
-							expect( err ).to.not.exist;
-						} )
-						.done( function () {
-							done();
-						} );
+					.then( function ( data ) {
+						expect( data ).to.exist;
+						expect( data ).to.be.an.array;
+					}, function ( err ) {
+						expect( err ).to.not.exist;
+					} )
+					.done( function () {
+						done();
+					} );
 			} );
 
 			//Todo: Improve the test ==> move to unit tests
 			it( 'should return only extensions of type <mashup-template>', function ( done ) {
 				qrs.extension.getInstalledMashupTemplates()
-						.then( function ( data ) {
-							expect( data ).to.exist;
-							expect( data ).to.be.an.array;
-						}, function ( err ) {
-							expect( err ).to.not.exist;
-						} )
-						.done( function () {
-							done();
-						} );
+					.then( function ( data ) {
+						expect( data ).to.exist;
+						expect( data ).to.be.an.array;
+					}, function ( err ) {
+						expect( err ).to.not.exist;
+					} )
+					.done( function () {
+						done();
+					} );
 			} );
 
 			describe( 'qrs.extension.upload', function () {
 
-
 				it( 'doesn\'t allow upload of non existing files', function ( done ) {
-					qrs.extension.upload( path.join(__dirname, './fixtures/extensions/qrs-ABCDEFGHIJKLMNOPQ.zip'))
-							.then( function ( data ) {
-								expect( data ).to.not.exist;
-							}, function ( err ) {
-								expect( err ).to.exist;
-								expect(err ).contains('File does not exist.');
-							})
-							.done( function (  ) {
-								done();
-							});
+					qrs.extension.upload( path.join( __dirname, './fixtures/extensions/qrs-ABCDEFGHIJKLMNOPQ.zip' ) )
+						.then( function ( data ) {
+							expect( data ).to.not.exist;
+						}, function ( err ) {
+							expect( err ).to.exist;
+							expect( err ).contains( 'File does not exist.' );
+						} )
+						.done( function () {
+							done();
+						} );
 
 				} );
 
 				it( 'doesn\'t allow upload of an extension with a different type than .zip', function ( done ) {
 
-					qrs.extension.upload( path.join(__dirname, './fixtures/extensions/qrs-sample.7z'))
-							.then( function ( data ) {
-								expect( data ).to.not.exist;
-							}, function ( err ) {
-								expect( err ).to.exist;
-								expect( err ).contains('Only .zip files can be uploaded.')
-							})
-							.done( function () {
-								done();
-							});
+					qrs.extension.upload( path.join( __dirname, './fixtures/extensions/qrs-sample.7z' ) )
+						.then( function ( data ) {
+							expect( data ).to.not.exist;
+						}, function ( err ) {
+							expect( err ).to.exist;
+							expect( err ).contains( 'Only .zip files can be uploaded.' )
+						} )
+						.done( function () {
+							done();
+						} );
 				} );
-			});
-
-
+			} );
 
 			/**
 			 * @todo: Returns "bad request" if extension is already existing.
@@ -183,14 +169,14 @@ describe.skip( 'qrs.extension', function () {
 			it( 'should allow upload of an extension with absolute path', function ( done ) {
 				var extFilePath = path.join( __dirname, './fixtures/extensions/qrs-sample.zip' );
 				qrs.extension.upload( extFilePath )
-						.then( function ( data ) {
-							expect( data ).to.exist;
-						}, function ( err ) {
-							expect( err ).to.not.exist;
-						} )
-						.done( function () {
-							done();
-						} );
+					.then( function ( data ) {
+						expect( data ).to.exist;
+					}, function ( err ) {
+						expect( err ).to.not.exist;
+					} )
+					.done( function () {
+						done();
+					} );
 			} );
 
 			describe( 'should allow deletion of an extension', function () {
@@ -215,6 +201,6 @@ describe.skip( 'qrs.extension', function () {
 				done();
 			} );
 
-		});
-	});
+		} );
+	} );
 } );
